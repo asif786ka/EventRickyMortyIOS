e67dbeb9776173cab305c7c741ee29aaf84b6344
“asifmacmini” <asif.kazmi@helloglobo.com>
2024-08-27 17:46:24 +0530

    Initial Commit

---
+++ EventRickyMorty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EventRickyMorty
+//
+//  Created by AsifMacMini on 27/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EventRickyMorty/EventRickyMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventRickyMortyApp.swift
+//  EventRickyMorty
+//
+//  Created by AsifMacMini on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct EventRickyMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
